74dace5779fbd7e59011fe4f0ece543e0f0abff5
saturno0 <lautaro.agustint@gmail.com>
2025-11-19 15:25:05 -0300

    Arreglamos el dispose para que se cierre la terminal cuando se cierra la ventana

--- core/src/main/java/escenas/Principal.java
+++ core/src/main/java/escenas/Principal.java
@@ -1,11 +1,20 @@
 package escenas;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
 public class Principal extends Game {
 	
     @Override
     public void create() {
         setScreen(new Menu(this));
+    }
+
+    @Override public void dispose() { 
+        Screen current = getScreen();
+        if (current != null) { 
+            current.dispose(); 
+        } 
+        super.dispose();
     }
 }
