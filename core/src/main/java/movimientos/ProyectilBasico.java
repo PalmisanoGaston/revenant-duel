8b80a8fb34ed0b8ad05ea46869a9472573ed7a2e
y0moGh <80994830+y0moGh@users.noreply.github.com>
2025-11-24 18:09:14 -0300

    fix: El icon de proyectil ahora se ve como proyectil
    Co-Authored-By: PalmisanoGaston <130399416+PalmisanoGaston@users.noreply.github.com>

--- core/src/main/java/movimientos/ProyectilBasico.java
+++ core/src/main/java/movimientos/ProyectilBasico.java
@@ -13,7 +13,7 @@ public class ProyectilBasico extends MovimientoProyectil {
     public ProyectilBasico(Body cuerpo, boolean ladoDerecho, World world, ProyectilManager proyectilManager, PersonajeBase personaje) {
         super(cuerpo, ladoDerecho, 10, world, proyectilManager,
                 "basico", "Proyectil Basico", 2f, personaje,0f,
-                new IconMovimiento(new Texture("movimientos/proyectilVolador.png")));
+                new IconMovimiento(new Texture("proyectil.png")));
      
     }
 }
